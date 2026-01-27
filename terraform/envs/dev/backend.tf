cdc54e26576bf87018fd80cc246480cbb0297774
Marcelo Oliveira <marcelo@marceloweb.info>
2026-01-27 16:35:44 -0300

    fix: pipeline

--- terraform/envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "terraform-states-by-githubactions"
-    key     = "example-app/dev/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
